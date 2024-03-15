067bed6695f7483f049a0e3080d1ef22eb27446e
Hossain Khan <hello@hossain.dev>
2024-03-14 21:24:32 -0400

    [ADDED] Testing with sample data

--- parser/build.gradle.kts
+++ parser/build.gradle.kts
@@ -1,6 +1,13 @@
 plugins {
   kotlin("jvm")
+
+  // Ktlint plugin
+  // https://github.com/pinterest/ktlint
   id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
+
+  // KSP plugin
+  // https://github.com/google/ksp/releases
+  id("com.google.devtools.ksp").version("1.9.23-1.0.19") // Or latest version of KSP
 }
 
 group = "dev.hossain.timeline"
@@ -11,10 +18,12 @@ repositories {
 }
 
 dependencies {
-  implementation("com.squareup.moshi:moshi:1.15.1")
-  implementation("com.squareup.moshi:moshi-kotlin:1.12.0")
+  implementation("com.squareup.moshi:moshi-kotlin:1.15.0")
+  ksp("com.squareup.moshi:moshi-kotlin-codegen:1.15.0")
   implementation("com.squareup.okio:okio:3.7.0")
   testImplementation("org.jetbrains.kotlin:kotlin-test")
+  // https://github.com/google/truth
+  testImplementation("com.google.truth:truth:1.4.2")
 }
 
 tasks.test {
