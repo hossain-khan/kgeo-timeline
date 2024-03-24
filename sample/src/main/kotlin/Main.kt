31af3b5b96d03baa192e7e8a7398a1ff583f2a48
Hossain Khan <hello@hossain.dev>
2024-03-23 20:18:07 -0400

    [REFACTOR] Use all semantic file parsing

--- sample/src/main/kotlin/Main.kt
+++ sample/src/main/kotlin/Main.kt
@@ -7,7 +7,18 @@ import java.io.File
 
 /**
  * Main entry point for the sample application that tests the Google Location History [Parser].
- * Drop your unzipped Google Location History JSON files in `sample/src/main/resources` and run to test app.
+ * Drop your unzipped Google Location History JSON files from `Takeout\Location History (Timeline)`
+ * into `sample/src/main/resources` and run to test app.
+ *
+ * Sample directory structure for `Takeout\Location History (Timeline)`:
+ * ```
+ * |- Semantic Location History
+ * |  |- 2022
+ * |  |  |- 2022_January.json
+ * |- Records.json
+ * |- Settings.json
+ * |- Timeline Edits.json
+ * ```
  */
 fun main() {
   println("Sample app for Google Location History Parser project.")
@@ -16,33 +27,34 @@ fun main() {
     println("Resources directory not found. Please create `sample/src/main/resources` and put your Google Location History JSON files.")
     return
   }
-
-  parseRecords(resourcesDir)
-  parseSemanticRecords(resourcesDir)
+  val parser = Parser()
+  parseRecords(resourcesDir, parser)
+  parseSemanticRecords(resourcesDir, parser)
 }
 
-private fun parseRecords(resourcesDir: File) {
-  val parser = Parser()
+private fun parseRecords(resourcesDir: File, parser: Parser) {
+
   val recordsFile = File(resourcesDir, "Records.json")
-  val buffer: BufferedSource = recordsFile.source().buffer()
-  val records = parser.parseRecords(buffer)
+  val bufferedSource: BufferedSource = recordsFile.source().buffer()
+  val records = parser.parseRecords(bufferedSource)
 
   println("Got records: ${records.locations.size} records.")
   println()
 }
 
-fun parseSemanticRecords(resourcesDir: File) {
-  val parser = Parser()
+fun parseSemanticRecords(resourcesDir: File, parser: Parser) {
+  val directory = File(resourcesDir, "Semantic Location History")
 
-  val directory = File(resourcesDir, "Location_History/2022/")
-  val files = directory.listFiles()
-  files?.forEach {
-    // List each files of directory
-    println("Parsing file: ${it.name}")
+  val semanticYears = directory.listFiles()
+  semanticYears?.forEach { yearDirectory ->
+    val files = yearDirectory.listFiles()
+    files?.forEach {
+      // List each files of directory
+      println("Parsing file: ${it.name}")
+      val bufferedSource: BufferedSource = it.source().buffer()
+      val semanticTimeline = parser.parseSemanticTimeline(bufferedSource)
 
-    val jsonText: String = it.readText()
-    val semanticTimeline = parser.parseSemanticTimeline(jsonText)
-
-    println("Got timeline items: ${semanticTimeline.timelineObjects.size}")
+      println("Got timeline items: ${semanticTimeline.timelineObjects.size}")
+    }
   }
 }
